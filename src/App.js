d918195fe351f834f8c044bbde7480efd1933cca
Ramazan Doğan <doganrmzn40@gmail.com>
2022-10-15 09:50:49 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [message, setMessage] = useState("Hello World");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Hello World 🤟🏻</h1>
+      <p>
+        It's my first React project to long time later 👈🏻 <br /> and im planning
+        to make a time counter app ⏱ <br />
+        <br /> lets do this 😉
+      </p>
+      <h4>Count: {count}</h4>
+      <div>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Increase
+        </button>
+        <button
+          onClick={() => {
+            setCount(count - 1);
+          }}
+        >
+          Decrease
+        </button>
+        <div>
+          <p>{message}</p>
+        </div>
+      </div>
     </div>
   );
 }
